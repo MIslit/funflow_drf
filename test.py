466e8c888bada73e849a78b13c37022ce176c165
Max Ivanov <imaks1464@gmail.com>
2024-12-07 00:19:42 +0300

    stoped tracking env

--- test.py
+++
@@ -1,7 +0,0 @@
-import os
-from pathlib import Path
-from decouple import config
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))).resolve().parents
-BASE_DIR = Path(__file__).parent
-
-print(BASE_DIR)
